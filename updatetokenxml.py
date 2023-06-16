bcd2a7c72b666cea5efc5dfc03b5061ed2cd012a
SlightlyCircuitous <71394296+SlightlyCircuitous@users.noreply.github.com>
2023-06-16 16:56:11 -0400

    Update updatetokenxml.py
    *Removes initial comment block in favor of having it in the readme

--- updatetokenxml.py
+++ updatetokenxml.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-v3.2 - Handle subtype logic with Try/Except instead of has_subtype
-Command line tool to compare a new set of tokens from Scryfall with an existing token XML. Pulls down a token set from Scryfall, appends reprinted tokens to a copy of the current XML file, and creates entries for new tokens in a separate XML file. Pulls the current large Scryfall hard link for picURLs.
-
-Does not handle:
---Filling out related or reverse-related fields
---Adding spaces to non-uniquely named tokens
-
-Treats tokens with new reminder text as new tokens.
-
-Run using 'python filename setcode /path/to/current/XML/file' or make the file executable with chmod and use './filename setcode /path/to/current/XML/file', when in the same folder as this script file.
-"""
-
 import requests
 import sys
 import time
